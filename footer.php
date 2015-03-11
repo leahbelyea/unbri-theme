70d95d9717770e0cd0e2b6b606e255f123183c22
n9i5a <leahbelyea@gmail.com>
2015-03-11 20:44:37 -0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+    </div>
+    <div class="clear"></div>
+    <div class="footer-trans"></div>
+  </div>
+  <div class="footer-wrapper">
+    <div class="footer">
+      <div class="footer-left">
+        <a class="doubleline" href="/contact">contact</a>
+      </div>
+      <div class="footer-right">
+        <a href="http://www.facebook.com/unbrockandice">
+          <img alt="facebook" src="<?php bloginfo('stylesheet_directory'); ?>/images/facebook.jpg"/>
+        </a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
